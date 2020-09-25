192f3860959b6ec151c3e9b09bdf7c835a45bcdb
Szabo Armin Andras <szabo.armin.prince@gmail.com>
2020-09-25 10:01:18 +0200

    JS óra

---
+++ 2020_10_09/script.js
@@ -0,0 +1,15 @@
+var myvar = setInterval(Time, 1000)
+
+function Time()
+{
+var TimeDate = new Date();
+var TimeHour = TimeDate.getHours(); 
+var TimeMin = TimeDate.getMinutes(); 
+var TimeSec = TimeDate.getSeconds(); 
+
+if(TimeHour < 10) TimeHour = "0" + TimeHour
+if(TimeMin < 10) TimeMin = "0" + TimeMin
+if(TimeSec < 10) TimeSec = "0" + TimeSec
+
+document.getElementById("timeOut").innerHTML = TimeHour + ":" + TimeMin + ":"+ TimeSec
+}
