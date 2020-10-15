6a1434ad1ec4d542ef69bb86e3052c62d891ddfe
Szabó Ármin András <szabo.armin.prince@gmail.com>
2020-10-16 01:12:09 +0200

    test.css befejezése

--- 2020_10_09/Test.js
+++ 2020_10_09/Test.js
@@ -14,10 +14,69 @@ if(TimeSec < 10) TimeSec = "0" + TimeSec
 document.getElementById("timeOut").innerHTML = TimeHour + ":" + TimeMin + ":"+ TimeSec
 }
 
-$(document).ready(function(){
-  $("#viruspopup").hover(function(){
+$(document).ready(function()
+{
+  $("#HDDpopup").hover(function()
+  {
+    $(".HDDpopup").css("visibility", "visible");
+  },
+  function()
+  {
+    $(".HDDpopup").css("visibility", "hidden");
+    
+  }
+  );
+
+  $("#SSDpopup").hover(function()
+  {
+    $(".SSDpopup").css("visibility", "visible");
+  },
+  function()
+  {
+    $(".SSDpopup").css("visibility", "hidden");
+    
+  }
+  );
+
+  $("#viruspopup").hover(function()
+  {
     $(".viruspopup").css("visibility", "visible");
-  },function(){
+  },
+  function()
+  {
     $(".viruspopup").css("visibility", "hidden");
-  });
-});
+    
+  }
+  );
+
+  $("#POSTpopup").hover(function()
+  {
+    $(".POSTpopup").css("visibility", "visible");
+  },
+  function()
+  {
+    $(".POSTpopup").css("visibility", "hidden");
+    
+  }
+  );
+}
+);
+
+function Test()
+{
+var points = 0;
+console.log(document.getElementById('hdd3').value)
+if(document.getElementById('hdd3').checked) points++;
+console.log(points);
+console.log(document.getElementById('ssd1').value)
+if(document.getElementById('ssd1').value == "Solid State Drive") points++;
+console.log(points);
+console.log(document.getElementById('virus2').value)
+if(document.getElementById('virus2').checked) points++;
+console.log(points);
+console.log(document.getElementById('POST2').value)
+if(document.getElementById('POST2').checked) points++;
+console.log(points);
+
+alert("Ennyi pontod lett:" + points);
+}
